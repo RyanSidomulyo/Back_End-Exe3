120b8c71f1ac48b6b1f9eeb323140947596dd817
JoanBokko <sidomulyo784@gmail.com>
2023-02-14 00:21:07 +0800

    menambahkan tugas 3

---
+++ about/member.js
@@ -0,0 +1,4 @@
+module.exports.id = () => 'S2200628'
+module.exports.nim = () => '11111110000000'
+module.exports.nama = () => 'Sidomulyo, Ryan Yedid-Yah'
+module.exports.nomorTelp = () => '082291349125'
